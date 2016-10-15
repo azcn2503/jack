6704e4ca795f70f379d9941d7ec07d17c4eab803
Aaron Cunnington <azcn2503@gmail.com>
2016-10-15 15:44:33 +0100

    Replaced example with a Google example.

--- src/example.js
+++ src/example.js
@@ -4,18 +4,14 @@ const jack = new Jack();
 
 let urls = [];
 for (let i = 374553; i < 374563; i += 1) {
-  jack.AddToHitList(`https://example.site/${i}`, [
+  jack.AddToHitList(`https://www.google.com`, [
     async () => {
-      const usernameField = await jack.driver.findElement(jack.By.css('input#UserName'));
-      const passwordField = await jack.driver.findElement(jack.By.css('input#Password'));
-      const submitButton = await jack.driver.findElement(jack.By.css('button#Log_In'));
-      await usernameField.sendKeys('username');
-      await passwordField.sendKeys('password');
-      await submitButton.click();
-    },
-    async () => {
-      // Something I should do after logging in
-      return true;
+      const searchField = await jack.driver.findElement(jack.By.css('input[type=text][name=q]'));
+      await searchField.sendKeys(`testing ${i}` + jack.Key.RETURN);
+      await jack.driver.wait(jack.until.elementLocated(jack.By.css('h3.r')));
+      const title = await jack.driver.findElement(jack.By.css('h3.r')).getAttribute('innerText');
+      console.log(title);
+      return title;
     }
   ]);
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import webdriver, { By } from "selenium-webdriver";
+import webdriver, { By, until, wait, Key } from "selenium-webdriver";
 import "babel-polyfill";
 
 export default class Jack {
   constructor(hitList = []) {
     this.driver = new webdriver.Builder().forBrowser('chrome').build();
     this.By = By;
+    this.until = until;
+    this.wait = wait;
+    this.Key = Key;
     this.hitList = hitList;
   }
 
@@ -17,22 +20,29 @@ export default class Jack {
       let prey = this.hitList[i];
       if (!prey.address || !prey.actions) { continue; }
       if (typeof(prey.actions) !== 'object') { prey.actions = [prey.actions]; }
+
       console.log('...');
       console.log(`Approaching ${prey.address}`);
+
       try {
         const result = await this.driver.get(prey.address);
       } catch (e) {
         console.error(e);
         continue;
       }
+
+      let actionResults = [];
+
       for (let j in prey.actions) {
         const action = prey.actions[j];
         try {
-          const actionResult = await action();
+          const actionResult = await action(actionResults, (data) => { actionResults.push(data); });
         } catch (e) {
           console.error(e);
+          actionResults.push(null);
           continue;
         }
+        
         console.log('Done!');
       }
       console.log(`Striking ${prey.address} off the list`);
